72e8bc42e70d1e656d23cce23f74b3d55e6c3d57
sahamoucktic <128138440+sahamoucktic@users.noreply.github.com>
2023-03-30 21:36:57 +0530

    Add files via upload

---
+++ Patt2.java
@@ -0,0 +1,8 @@
+class Patt2{
+public static void main(String args[]){
+int alpha=65;
+for(int i=0;i<=4;i++){
+for(int j=0;j<=i;j++){
+System.out.print((char)(alpha+j)+" ");}
+System.out.println();
+}}}
