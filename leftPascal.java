79d9129b49f52293ea72b190160dd32ac32f236d
sahamoucktic <128138440+sahamoucktic@users.noreply.github.com>
2023-04-02 11:53:43 +0530

    Add files via upload

---
+++ leftPascal.java
@@ -0,0 +1,20 @@
+class leftPascal {
+  public static void main(String[] args) {
+    // left pasal triangle
+    int size = 5;
+
+    for (int i = 1; i <= size; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    
+    for (int i = 1; i <= size - 1; i++) {
+      for (int j = 0; j < size - i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
