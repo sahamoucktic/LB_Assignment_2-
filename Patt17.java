03e9ee072eccdd65dbf5c0db532992df7e1c5aba
sahamoucktic <128138440+sahamoucktic@users.noreply.github.com>
2023-04-02 11:00:58 +0530

    Add files via upload

---
+++ Patt17.java
@@ -0,0 +1,13 @@
+class Patt17{
+public static void main(String [] args){
+int count=1;
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++){
+System.out.print(count+" ");
+count++;
+}
+System.out.println();
+}
+}
+}
